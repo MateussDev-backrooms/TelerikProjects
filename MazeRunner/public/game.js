c631451d0dfcab931739593ec6505567f0a0f354
MateussDev-backrooms <thisismateussdev@gmail.com>
2023-01-22 12:10:09 +0200

    maze runner update

--- MazeRunner/public/game.js
+++ MazeRunner/public/game.js
@@ -4,7 +4,14 @@ let box, coin, cylinder;
 
 let maze, maze_walls;
 
+let flashlight;
+
+let player_move_f = true, player_move_b = true, player_move_l = true, player_move_r = true;
+let player_moving_f = false, player_moving_b = false, player_moving_l = false, player_moving_r = false;
+
 let keyDownArr = Array(256).fill(0); // Generates 256 long empty array for all keyboard buttons
+
+let mouseX, mouseY;
 
 function init() {
   setupInput();
@@ -50,13 +57,14 @@ function init() {
   // Create ground
   ground = new THREE.Mesh(box_geom, grass_mat);
   ground.scale.set(64, 64, 1);
+  ground.receiveShadow = true;
 
   //setup default geometries
   box = new THREE.BoxGeometry(1, 1, 1);
   coin = new THREE.CylinderGeometry(0.2, 0.2, 0.05, 12);
 
   maze = [];
-  maze = generateRandomMazeMap(64, 64, 25);
+  maze = generateRandomMazeMap(64, 64, 25, 4);
   maze_walls = [];
   let maze_wall_material = new THREE.MeshPhongMaterial({
     color: "white",
@@ -76,6 +84,8 @@ function init() {
           j - maze.length / 2 + 0.5,
           1
         );
+        maze_walls[i][j].castShadow = true;
+        maze_walls[i][j].receiveShadow = true;
         scene.add(maze_walls[i][j]);
       } else if (maze[i][j] == 2) {
         maze_walls[i][j] = new THREE.Mesh(coin, coin_material);
@@ -84,6 +94,8 @@ function init() {
           j - maze.length / 2 + 0.5,
           1
         );
+        maze_walls[i][j].castShadow = true;
+        maze_walls[i][j].receiveShadow = true;
         maze_walls[i][j].rotation.z = i * j;
         scene.add(maze_walls[i][j]);
       } else {
@@ -91,19 +103,25 @@ function init() {
     }
   }
   // Point light
-  const l1 = new THREE.PointLight("white", 0.4);
-  l1.position.set(-1, -2, 4);
 
   // Ambient light
   const l2 = new THREE.AmbientLight("yellow", 0.3);
+  flashlight = new THREE.SpotLight("white", 5, 5, Math.PI/8, 1)
+  flashlight.position.set( camera.position.x, camera.position.y, camera.position.z );
+  flashlight.up.set(0, 0, 1)
+  flashlight.rotation.x = Math.PI/2
+
+  flashlight.castShadow = true;
+
+  flashlight.lookAt(0, 9999, 5)
+
+  const spotLightHelper = new THREE.SpotLightHelper( flashlight );
+  scene.add( spotLightHelper );
 
   // Directional light
-  const l3 = new THREE.DirectionalLight("white", 0.7);
-  l3.position.set(-4, -4, 5);
-  l3.lookAt(0, 0, 0);
 
   // Add all objects to scene
-  scene.add(l1, l2, l3, ground);
+  scene.add(l2, flashlight, ground);
 }
 
 function update() {
@@ -119,27 +137,37 @@ function update() {
       }
     }
   }
+
+  flashlight.position.set(camera.position.x, camera.position.y, camera.position.z)
+  flashlight.rotation.set(camera.rotation.x, camera.rotation.y, camera.rotation.z)
+
   updatePlayer();
 }
 
-function generateRandomMazeMap(w, h, wall_probability) {
+function generateRandomMazeMap(w, h, wall_probability, spawnSize) {
   let result = [];
   for (let x = 0; x < w; x++) {
     result[x] = [];
     for (let y = 0; y < h; y++) {
-      if (Math.random() < wall_probability / 100) {
-        result[x][y] = 1;
-      } else {
-        if (Math.random() < 0.5) {
-          //spawn coin
-          result[x][y] = 2;
-        } else {
-          //empty
+      if(x > w/2-Math.round(spawnSize/2) && x > w/2+Math.round(spawnSize/2) &&
+         y > h/2-Math.round(spawnSize/2) && y > h/2+Math.round(spawnSize/2)) {
           result[x][y] = 0;
+      } else {
+        if (Math.random() < wall_probability / 100) {
+          result[x][y] = 1;
+        } else {
+          if (Math.random() < 0.5) {
+            //spawn coin
+            result[x][y] = 2;
+          } else {
+            //empty
+            result[x][y] = 0;
+          }
         }
       }
     }
   }
+  console.log(result)
   return result;
 }
 
@@ -151,37 +179,104 @@ function updatePlayer() {
     camera.position.z -= 0.098;
   }
 
+  let phi = -((mouseX - window.innerWidth/2)/5) * Math.PI / 180
+  let theta = clamp(((mouseY - window.innerHeight/5)/2) * Math.PI / 180, -Math.PI, Math.PI)
+  let r = 1
+
+  let lookat_x = r * Math.cos(phi) * Math.sin(theta);
+  let lookat_y = r * Math.sin(phi) * Math.sin(theta);
+  let lookat_z = r * Math.cos(theta);
+
+  camera.lookAt(camera.position.x + lookat_x, camera.position.y + lookat_y, camera.position.z + lookat_z)
+
+
   //Movement
-  if (keyDownArr[87]) {
+  if (keyDownArr[87] && player_move_f) {
     //W
-    camera.position.y += Math.cos(camera.rotation.y) * 0.05;
-    camera.position.x -= Math.sin(camera.rotation.y) * 0.05;
+    camera.position.y += Math.cos(phi - Math.PI / 2) * 0.05;
+    camera.position.x -= Math.sin(phi - Math.PI / 2) * 0.05;
   }
-  if (keyDownArr[83]) {
+  if (keyDownArr[83] && player_move_b) {
     //S
-    camera.position.y -= Math.cos(camera.rotation.y) * 0.05;
-    camera.position.x += Math.sin(camera.rotation.y) * 0.05;
+    camera.position.y += Math.cos(phi + Math.PI / 2) * 0.05;
+    camera.position.x -= Math.sin(phi + Math.PI / 2) * 0.05;
   }
-  if (keyDownArr[65]) {
+  if (keyDownArr[65] && player_move_l) {
     //A
-    camera.position.y += Math.cos(camera.rotation.y + Math.PI / 2) * 0.05;
-    camera.position.x -= Math.sin(camera.rotation.y + Math.PI / 2) * 0.05;
+    camera.position.y += Math.cos(phi) * 0.05;
+    camera.position.x -= Math.sin(phi) * 0.05;
   }
-  if (keyDownArr[68]) {
+  if (keyDownArr[68] && player_move_r) {
     //D
-    camera.position.y += Math.cos(camera.rotation.y - Math.PI / 2) * 0.05;
-    camera.position.x -= Math.sin(camera.rotation.y - Math.PI / 2) * 0.05;
+    camera.position.y += Math.cos(phi + Math.PI) * 0.05;
+    camera.position.x -= Math.sin(phi + Math.PI) * 0.05;
   }
 
-  if (keyDownArr[39]) {
-    //Right arrow
-    camera.rotation.y -= 0.05;
-  }
-  if (keyDownArr[37]) {
-    //Left arrow
-    camera.rotation.y += 0.05;
+  if(keyDownArr[32]) {
+    camera.position.set(0, 0, 5)
   }
 
+  //bind movement to vars
+  player_moving_f = keyDownArr[87];
+  player_moving_b = keyDownArr[83];
+  player_moving_l = keyDownArr[65];
+  player_moving_r = keyDownArr[68];
+
+  // console.log(camera.rotation.z)
+
+  
+
+  
+
+  for (let i = 0; i < maze_walls.length; i++) {
+    for (let j = 0; j < maze_walls[i].length; j++) {
+      if(maze[i][j] != 0) {
+        if(areColliding2D(i - maze.length / 2 + 0.5, j - maze.length / 2 + 0.5, 1, 1, camera.position.x, camera.position.y, 0.25, 0.25)) {
+          if(maze[i][j] == 1) {
+
+            //block movement based off direction
+            if(player_moving_f) {
+
+              //Move back
+              camera.position.y -= Math.cos(camera.rotation.z) * 0.05;
+              camera.position.x += Math.sin(camera.rotation.z) * 0.05;
+            }
+            if(player_moving_b) {
+
+              //Move forward
+              camera.position.y += Math.cos(camera.rotation.z) * 0.05;
+              camera.position.x -= Math.sin(camera.rotation.z) * 0.05;
+            }
+            if(player_moving_l) {
+
+              //Move right
+              camera.position.y += Math.cos(camera.rotation.z - Math.PI / 2) * 0.05;
+              camera.position.x -= Math.sin(camera.rotation.z - Math.PI / 2) * 0.05;
+            }
+            if(player_moving_r) {
+
+              //Move Left
+              camera.position.y += Math.cos(camera.rotation.z + Math.PI / 2) * 0.05;
+              camera.position.x -= Math.sin(camera.rotation.z + Math.PI / 2) * 0.05;
+            }
+
+          } else if(maze[i][j] == 2) {
+            maze[i][j] = 0;
+            scene.remove(maze_walls[i][j])
+            player_move_f = true;
+            player_move_b = true;
+            player_move_l = true;
+            player_move_r = true;
+          } else {
+            player_move_f = true;
+            player_move_b = true;
+            player_move_l = true;
+            player_move_r = true;
+          }
+        } 
+      }
+    }
+  }
   
 }
 
@@ -191,6 +286,10 @@ function setupInput() {
   });
   window.addEventListener("keyup", ($event) => {
     keyDownArr[$event.keyCode] = 0;
+  });
+  window.addEventListener("mousemove", ($event) => {
+    mouseX = $event.pageX;
+    mouseY = $event.pageY;
   });
 }
 
@@ -206,3 +305,13 @@ function areColliding2D(Ax, Ay, Awidth, Aheight, Bx, By, Bwidth, Bheight) {
     }
     return 0;
 };
+
+function clamp(x, min, max) {
+  if(x < min) {
+    return min;
+  } else if(x > max) {
+    return max;
+  } else {
+    return x;
+  }
+}
